fd93e393873412e4e41175b9c34662dd62644ab8
Rinatka777 <rinatsharifullin@gmail.com>
2025-10-26 19:53:23 +0200

    added reap and startreaploop

--- src/pokecache.ts
+++ src/pokecache.ts
@@ -5,6 +5,9 @@ export type CacheEntry<T> = {
 
 export class Cache{
     #cache = new Map<string, CacheEntry<any>>();
+    #reapIntervalId: NodeJS.Timeout | undefined = undefined;
+    #interval: number;
+
     add<T>(key: string, val: T): void{
         const entry: CacheEntry<T> = {
             createdAt: Date.now(),
@@ -16,5 +19,23 @@ export class Cache{
         const entry= this.#cache.set(key);
         if(!entry) return undefined;
         return entry as CacheEntry<T>;
+    }
+    #reap(): void{
+        const expirationTime = Date.now() - this.#interval;
+        for(const[key,entry] of this.#cache){
+            if(entry.createdAt<expirationTime){
+                this.#cache.delete(key);
+            }
+        }
+    }
+    #startReapLoop():void{
+        setInterval(this.#reap, this.#interval, this.#reapIntervalId);
+    }
+    constructor(interval: number){
+        this.#interval = interval;
+        this.#startReapLoop();
+    }
+    stopReapLoop():void{
+
     }
 }
